46d487c01122cc33740d3f4195da013a3257253c
Rob <robsc801@gmail.com>
2022-01-25 14:50:53 +1300

    set up prop

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,8 +3,22 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
-function App() {
-  return <h2>menu project setup</h2>;
+function App() { 
+  const [menuItems, setMenuItems] =useState(items)
+  const [categories,SetCategories] = useState ([])
+  
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+        <h2>our menu</h2>
+        <div className='underline'></div>
+        </div>
+        <Categories/>
+        <Menu  items={menuItems}/>
+      </section>
+    </main>
+  ) 
 }
 
 export default App;
