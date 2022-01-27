e92a4200dafc766a3ed059ed717448a4c46a24d0
Rob <robsc801@gmail.com>
2022-01-27 15:57:05 +1300

    set state and fetch Jobs

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -3,7 +3,30 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
+
 function App() {
+  const [loading,setLoading] = useState(true)
+  const [jobs,setJobs] = useState=([])
+  const [value,setValue] = useState(0)
+
+  const fetchJobs = async () => {
+    const response = await fetch(url)
+    const newJobs = await response.json()
+    setJobs(newJobs)
+    setLoading(false)
+  }
+
+  useEffect(()=> {
+  fetchJobs()
+}, [])
+
+if(loading) {
+  return ( 
+  <section className='section loading'>
+  <h1>loading...</h1>
+  </section>
+  )
+}  
   return <h2>tabs project setup</h2>
 }
 
