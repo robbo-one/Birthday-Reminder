e003e62eda489b49d75f6877ff2ebb1a297e7f17
Rob <robsc801@gmail.com>
2022-01-27 16:36:38 +1300

    add buttons

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -27,7 +27,37 @@ if(loading) {
   </section>
   )
 }  
-  return <h2>tabs project setup</h2>
+const {company, dates, duties, title} = jobs[value]
+return ( 
+<section className='section'>
+  <div className='title'>
+    <h2>experience</h2>
+    <div className='underline'></div>
+    </div>
+  <div className='jobs-center'>
+    {/* btn container */}
+    <div className='btn-container'>
+    {jobs.map((item, index) => {
+      return <button>{item.company}</button>
+    })}
+    </div>
+        {/* job info */}
+        <article className='job-info'>
+      <h3>{title}</h3>
+      <h4>{company}</h4>
+      <p className='job-date'>{dates}</p>
+      {duties.map((duty, index) => {
+        return (
+         <div key={index} className='job-desc'>
+          <FaAngleDoubleRight className='job-icon'>
+            </FaAngleDoubleRight>
+            <p>{duty}</p>
+            </div>
+            )
+      })}
+</article>
+</div>
+</section>
+  )
 }
-
 export default App
